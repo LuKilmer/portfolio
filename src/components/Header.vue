137ac37eb83fe8599863cdf9681f5d654f5461cc
nonluck <luiskilmerluk1221@gmail.com>
2023-12-23 22:35:45 -0300

    fix: header ajustado

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,58 +4,141 @@ import { useDark, useToggle} from '@vueuse/core';
 const isDark = useDark()
 const toggleDark = useToggle(isDark);
 
+function callToggle(){
+  toggleDark();
+}
+
+
+/*
+<label class="switch">
+  <input type="checkbox" @click="toggleDark()">
+  <span class="slider"></span>
+</label>
+
+*/
 </script>
 <template>
-  <header id="header" >
- 
-    <h1 id="title-name">&lt;LuKilmer&frasl;&gt;</h1>
+  <header id="header">
     <ul>
       <li><a href="#id">Home</a></li>
+      <li><a href="#id">Skills</a></li>
       <li><a href="#id">Projects</a></li>
       <li><a href="#id">About</a></li>
-      <li><a href="#id">Articles</a></li>
       <li>
-        <label class="switch">
-          <input type="checkbox" @click="toggleDark()">
-          <span class="slider"></span>
-        </label>
+        <button  id="dark-mode-button" @click="callToggle()">{{ isDark ? '☀️' : '🌙' }}</button>
       </li>
     </ul>
-
   </header>
+  <header id="header-mobile">
+    <div id="div-dark-mode">
+      <button  id="dark-mode-button" @click="callToggle()">{{ isDark ? '☀️' : '🌙' }}</button>
+    </div>
+    <div id="menu-mobile">
+      <button ><img src="../assets/menu.png"></button>
+    </div>
+  </header>
+
 </template>
 
 <style scoped>
+  #dark-mode-button{
+    cursor:pointer;
+    padding-top:2px;
+    font-size: larger;
+    background: none;
+    border: none;
+  }
+  #header-mobile{
+    display:none;
+  }
+
+  #dark-mode-button:hover{
+    border:none;
+  }
+
   #title-name{
     font-size: 1.6em;
-    margin-top:12px;
+    margin-top:13px;
   }
+  
+  .dark a{
+    color:rgb(231, 229, 229);;
+  }
+  .dark a:hover{
+    color:rgb(127, 21, 214);
+  }
+  #header{
+      position: absolute;
+      z-index: 100;
+      width: 100%;
+      height: 55px;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-around;
+      
+      background-color: rgb(246, 246, 246);
+      border: 1px 1px 0px 0px solid black;
+      box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 10px;
+    }
   .dark #header{
-    background-color:red;
+      color:rgb(231, 229, 229);
+      background-color:rgb(17, 17, 17);
+      border: 1px 1px 0px 0px solid black;
+      box-shadow: rgba(68, 68, 68, 0.35) 0px 5px 10px;
   }
- #header{
+  @media screen and (max-width: 400px) {
+  /* Estilos para telas menores que 600px */
+  #header {
+    height: 60px;
+    flex-direction: column;
+    justify-content: center;
+    text-align:center;
+    display: none;
+  }
+  #header-mobile{
+    display: flex;
+  }
+  ul{
+    margin:auto;
+  }
+  #header-mobile{
+    display:flex;
+    justify-content: space-between;
     position: absolute;
     z-index: 100;
     width: 100%;
-    height: 60px;
+    height: 55px;
     display: flex;
     flex-direction: row;
-    justify-content: space-around;
-    
+    padding:5px 0px;
     background-color: rgb(246, 246, 246);
     border: 1px 1px 0px 0px solid black;
     box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 10px;
   }
-  @media screen and (max-width: 600px) {
-  /* Estilos para telas menores que 600px */
-  #header {
-    height: 100px;
-    flex-direction: column;
-    justify-content: center;
-    text-align:center;
+  .dark #header-mobile {
+    color:rgb(231, 229, 229);
+      background-color:rgb(17, 17, 17);
+      border: 1px 1px 0px 0px solid black;
+      box-shadow: rgba(68, 68, 68, 0.35) 0px 5px 10px;
   }
-  ul{
-    margin:auto;
+
+  #header-mobile img{
+    width:40px;
+  }
+  #header-mobile button{
+    max-width: 100%; /* Garante que a imagem não ultrapasse a largura da div */
+    max-height: 100%; 
+  }
+  #div-dark-mode{
+    margin:10px;
+  }
+  #dark-mode-button{
+    cursor:pointer;
+    padding-top:2px;
+    font-size: 30px;
+  }
+  #menu-mobile{
+    margin:5px;
   }
 }
 
@@ -65,7 +148,7 @@ const toggleDark = useToggle(isDark);
     color:rgb(23, 23, 23);
   }
   a:hover{
-    color:rgb(17, 167, 197);
+    color:rgb(25, 193, 226);
     text-decoration:underline;
   }
 
@@ -78,57 +161,11 @@ const toggleDark = useToggle(isDark);
   }
   li{
     display: block;
+    
     float: left;
-    padding:0px 10px;
+    padding-inline: 10px;
     text-align:center;
   }
-  .switch {
-    position: relative;
-    display: inline-block;
-    width: 54px;
-    height: 34px;
-  }
-
-  .switch input {
-    opacity: 0;
-    width: 0;
-    height: 0;
-  }
-
-  .slider {
-    position: absolute;
-    cursor: pointer;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: #ccc;
-    transition: .4s;
-    border-radius: 34px;
-  }
-
-  .slider:before {
-    position: absolute;
-    content: "";
-    height: 26px;
-    width: 26px;
-    left: 4px;
-    bottom: 4px;
-    background-color: white;
-    transition: .4s;
-    border-radius: 50%;
-  }
-
-  input:checked + .slider {
-    background-color: #131313;
-  }
-
-  input:focus + .slider {
-    box-shadow: 0 0 1px #181818;
-  }
-
-  input:checked + .slider:before {
-    transform: translateX(20px);
-  }
+  
 </style>
 
